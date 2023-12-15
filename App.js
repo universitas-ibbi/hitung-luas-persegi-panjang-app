3247cfc97766bc9f2bfef60ba5e3210e7e68b878
iamgymie <6257063+gymie@users.noreply.github.com>
2023-12-15 11:15:51 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  StatusBar,
+  Button,
+} from "react-native";
 
 export default function App() {
+  const [panjang, setPanjang] = useState(0);
+  const [lebar, setLebar] = useState(0);
+  const [luas, setLuas] = useState(0);
+
+  const hitungLuas = () => {
+    const p = parseInt(panjang);
+    const l = parseInt(lebar);
+    const luas = p * l;
+    setLuas(luas);
+
+    alert("Luas Persegi Panjang = " + luas);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Hitung Luas Persegi Panjang</Text>
+      <Text style={styles.tulisan}>Panjang</Text>
+      <TextInput
+        keyboardType="numeric"
+        style={styles.input}
+        placeholder="Masukkan Panjang"
+        onChangeText={setPanjang}
+      />
+      <Text style={styles.tulisan}>Lebar</Text>
+      <TextInput
+        keyboardType="numeric"
+        style={styles.input}
+        placeholder="Masukkan Lebar"
+        onChangeText={setLebar}
+      />
+      <Button title="Hitung" onPress={hitungLuas} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
+    padding: 10,
+  },
+  heading: {
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  tulisan: {
+    fontSize: 25,
+  },
+  input: {
+    marginTop: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
   },
 });
